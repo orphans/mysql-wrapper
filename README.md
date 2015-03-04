d42ee2cdfa851d31ed7d4b3065b7c42b028f7d1d
Ed Jeavons <eddie@edjeavons.co.uk>
2015-03-04 19:49:24 +0000

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,165 @@
-# mysql-wrapper
-A database wrapper class to help reduce code and support rapid development
+# A Simple MySQL/MariaDB Wrapper
+
+A database class that we like to use within small rapid development tasks. It won't suit everyone but resonates with the way we like to interact with the MySQL/MariaDB.
+
+The class utilises the PHP Data Objects (PDO) extension and the primary aim is to reduce repetitive lines of code.
+
+## Installation
+
+### Composer
+
+This is the preferred method of installation.
+
+Add the following to your `composer.json` file:
+
+    {
+      "require" : {
+        "orphans/mysql-wrapper" : "0.1.*"
+      }
+    }
+
+Then install/update your composer project as normal. Remember to include the Composer autoloader:
+
+    require 'vendor/autoload.php';
+
+### Git
+
+You can also include this in any Git project as follows:
+
+    git clone https://github.com/orphans/mysql-wrapper.git
+    git submodule init
+    git submodule update
+
+Remembering that you will need to include `mysql_wrapper.class.php` in your project's code.
+
+### Unmanaged
+
+If you would rather not have any externally referenced code in your project you may simply download the `mysql_wrapper.class.php` file and include it in your project. This isn't the preferred way though.
+
+## Setup
+
+The code itself will be better documented later but for now the following examples illustrate various use cases.
+
+### Connect
+
+Create an instance with the following snippet:
+
+    $db = new MYSQL_WRAPPER();
+    $db->connect(array(
+    	'host' => '127.0.0.1', // hostname or IP
+    	'port' => '3306', // optional, defaults to 3306
+    	'username' => 'some_user',
+    	'password' => 'some_pass',
+    	'database' => 'some_db'
+    ));
+
+or, for a socket:
+
+    $db = new MYSQL_WRAPPER();
+    $db->connect(array(
+    	'socket' => '/path/to/mysql.sock',
+    	'username' => 'some_user',
+    	'password' => 'some_pass',
+    	'database' => 'some_db'
+    ));
+
+### Singleton model
+
+Although it has potential limitations for a DB class (no multiple connections), in the vast majority of cases this a convenient way to manage things.
+
+So if you you need to access the class where it's outside of the current scope use the following code rather than globalising the instance or passing it between functions.
+
+    $db = MYSQL_WRAPPER::get_singleton();
+
+## Query Helpers
+
+Queries are send/received as arrays where practical, but not at the expense of flexibility. So we do not shy away from writing SQL where that makes more sense; most notably for SELECT queries.
+
+When writing queries with user input that requires sanitisation you can use placeholders `:placeholder` then supply the value in a subsequent array parameter. That will make more sense in the examples below!
+
+If you supply the string value `'NOW()'` or `'NULL'` in an INSERT or UPDATE it will be converted into the special meaning when sent to MySQL.
+
+### Selects
+
+Selects return results as an array, or FALSE on failure.
+
+There are two methods `select()` and `select_single()`. Both work in the same way except the latter only ever returns a single-dimension array containing the first result.
+
+    $db->select("SELECT * FROM `orders`");
+
+    $db->select("SELECT * FROM `users` WHERE `name` = :name", [ 'name' => 'John Smith' ]);
+
+### Inserts
+
+Insert operations are done by sending a table reference and an array of fields => values.
+
+This method will return the new row's ID, or else FALSE on failure.
+
+    $db->insert('users', [
+    	'name' => 'John Smith',
+    	'email' => 'john.smith@somedomain.com',
+    	'last_updated' => 'NOW()',
+    ]);
+
+You can tell MySQL to ignore errors by adding an extra TRUE parameter to the end of the method call.
+
+    $db->insert('users', [
+    	'name' => 'John Smith',
+    	'email' => 'john.smith@somedomain.com',
+    	'last_updated' => 'NOW()',
+    ], TRUE);
+
+
+### Updates
+
+Updates work in a similar way to inserts except you pass a WHERE clause too. The first part of the clause (parameter 3) is the actual where clause, with placeholders used for sanitisation as described above.
+
+This method will return TRUE or FALSE depending on outcome status.
+
+    $db->update('users', [
+    	'email' => 'john.smith@somedomain.com',
+    	'last_updated' => 'NOW()',
+    ], "`id` = :id", [
+    	'id' => 1
+    ]);
+
+### Insert or update on duplicate
+
+Same as an insert operation but with a second array of update fields if a duplicate row already exists.
+
+This method will return TRUE or FALSE depending on outcome status.
+
+    $db->insert_or_update('users', [
+    	'name' => 'John Smith',
+    	'email' => 'john.smith@somedomain.com',
+    	'last_updated' => 'NOW()',
+    ], [
+    	'name' => 'John Smith',
+    	'last_updated' => 'NOW()',
+    ]);
+
+### Deletions
+
+Just needs a table name and a WHERE clause which uses placeholders used for sanitisation as described above.
+
+This method will return TRUE or FALSE depending on outcome status.
+
+
+    $db->delete('users', "`id` = :id", [
+    	'id' => 1
+    ]);
+
+
+## Other Queries
+
+For all other queries you can call the `query()` method which accepts a SQL query and an array of :variable replacements. See [this page](http://php.net/manual/en/pdostatement.execute.php) for a reminder of how these replacements work.
+
+    $db->query('ALTER TABLE `users` ADD COLUMN ...');
+
+## Closing Notes
+
+There are still some special purpose solutions in the class for converting date formats. These are not documented because they need generalising before they're more useful to all.
+
+Logging options are also unfinished and undocumented for now.
+
+Development will continue as new requirements (or shortfalls!) surface.
---
+++ mysql_wrapper.class.php
@@ -0,0 +1,352 @@
+<?php
+
+/*
+ * Singleton class - rather than globalising the object instance you should call
+ * $var = DATABASE::get_singleton() when it's needed but not accessible.
+ *
+ * Strings 'NOW()' and 'NULL' will be converted to MySQL keywords when found in INSERT and UPDATE queries
+ */
+
+class DATABASE {
+
+	private $dsn = NULL;
+	private $dbh = NULL;
+	private $log = array();
+	private $log_errors = FALSE;
+	private $log_queries = FALSE;
+	private $num_queries = 0;
+	private static $instance = NULL;
+
+	/*
+	 * !CLASS FUNCTIONALITY
+	 */
+
+	public function __construct() {
+	}
+
+	private function __clone() {
+	}
+
+	public static function get_singleton() {
+		if (self::$instance == NULL) {
+			self::$instance = new DATABASE();
+		}
+		return self::$instance;
+	}
+
+
+	/*
+	 * !CONNECTION
+	 *
+	 * $settings is an array containing the following key/value pairs:
+	 *
+	 *  [host]     => hostname or ip address
+     *  [port]     => port number (defaults to 3306)
+     *    or
+     *  [socket]   => path to mysql socket
+     *
+     *  [username] => mysql username (required)
+     *  [password] => mysql password (required)
+     *  [database] => mysql database (required)
+     *
+     *  [charset]  => chartset (defaults to utf8)
+	 */
+
+	function connect($settings = FALSE) {
+		try {
+
+			// build dsn
+			$this->dsn  = 'mysql:';
+			$options = array();
+
+			// host address (address or socket)
+			if (isset($settings['socket']) && !empty($settings['socket'])) {
+				$this->dsn .= 'unix_socket=' . $settings['socket'];
+			} else {
+				$this->dsn .= 'host=' . $settings['host'];
+				if (isset($settings['port']) && !empty($settings['port'])) {
+					$this->dsn .= ';port=' . $settings['port'];
+				}
+			}
+
+			// database name
+			$this->dsn .= ';dbname=' . $settings['database'];
+
+			// charset (default to utf8)
+			if (isset($settings['charset']) && !empty($settings['charset'])) {
+				$this->dsn .= ';charset=' . $settings['charset'];
+				$options[PDO::MYSQL_ATTR_INIT_COMMAND] = 'SET NAMES ' . $settings['charset'];
+			} else {
+				$this->dsn .= ';charset=utf8';
+				$options[PDO::MYSQL_ATTR_INIT_COMMAND] = 'SET NAMES utf8';
+			}
+
+			$this->dbh = new PDO($this->dsn, $settings['username'], $settings['password'], $options);
+			$this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		} catch(PDOException $e) {
+			echo "Cannot connect to " . $this->dsn . "\n";
+			echo $e->getMessage() . "\n";
+			exit();
+		}
+	}
+
+
+	/*
+	 *  !CORE QUERIES
+	 *
+	 *  TO DO: Restrict runctions to expected queries
+	 *         INSERT ... ON DUPLICATE UPDATE
+	 *         DELETE
+	 */
+
+	/*
+	 *  Used by all querying functions to prepare and execute the statement, though it can be
+	 *  called directly.
+	 */
+	function query($sql, $vars) {
+
+		$sth = $this->dbh->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		if (is_array($vars) && sizeof($vars)) {
+			$result = $sth->execute($vars);
+		} else {
+			$result = $sth->execute();
+		}
+		$this->num_queries++;
+		if ($this->log_queries) {
+			$this->log_append($sth->queryString);
+		}
+
+		// must have errored (note that most errors will be caught in dbh->prepare and will cause an exception so this is only for edge cases)
+		if (!$result) {
+			$error = $sth->errorInfo();
+			$this->record_error($sth->queryString, $vars, $error[2]);
+		}
+
+		return $sth;
+	}
+
+
+	/*
+	 *  Runs standard SELECT queries and returns an associative array containing all results.
+	 */
+	function select($sql, $vars = array()) {
+		$sth = $this->query($sql, $vars);
+		return $sth->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+	/*
+	 *  Returns a single result as an associate array. Users are expected to write 'LIMIT 1' in there
+	 *  own queries as best practice but if not this function will add it to save DB load.
+	 */
+	function select_single($sql, $vars = array()) {
+		if (!preg_match('/limit\s1/i', substr(trim($sql), -7))) {
+			$sql .= ' LIMIT 1';
+		}
+		$sth = $this->query($sql, $vars);
+		return $sth->fetch(PDO::FETCH_ASSOC);
+	}
+
+	/*
+	 *  Provide table name and associate array of field/value pairs
+	 *  Returns inserted ID or FALSE on failure
+	 */
+	function insert($table, $insert_fields, $ignore = FALSE) {
+		if (sizeof($insert_fields)) {
+			$vars = array();
+			$sql = "INSERT ";
+			if ($ignore === TRUE) {
+				$sql .= "IGNORE ";
+			}
+			$sql .= "INTO `$table` SET ";
+			foreach ($insert_fields as $k=>$v) {
+				// treat certain MySQL functions as safe
+				if ($v === 'NOW()') {
+					$sql .= "`$k` = NOW(), ";
+				} else if ($v === 'NULL') {
+					$sql .= "`$k` = NULL, ";
+				} else {
+					$sql .= "`$k` = :$k, ";
+					$vars[":$k"] = $v;
+				}
+			}
+			$sql = substr($sql, 0, -2);
+			if ($sth = $this->query($sql, $vars)) {
+				return $this->dbh->lastInsertId();
+			}
+
+		}
+		return FALSE;
+	}
+
+	/*
+	 *  Provide table name and associate array of field/value pairs
+	 *  Enforces a where clause for protection (practically, it's always wanted anyway)
+	 *  Where clause fields are auto-prepended with __where__ to prevent conflicts with the update fields
+	 *  Returns FALSE on failure
+	 */
+	function update($table, $update_fields, $where_clause = false, $where_fields = false) {
+		if ($where_clause && sizeof($update_fields)) {
+			$vars = array();
+			$sql = "UPDATE `$table` SET ";
+			foreach ($update_fields as $k=>$v) {
+				// treat certain MySQL functions as safe
+				if ($v === 'NOW()') {
+					$sql .= "`$k` = NOW(), ";
+				} else if ($v === 'NULL') {
+					$sql .= "`$k` = NULL, ";
+				} else {
+					$sql .= "`$k` = :$k, ";
+					$vars[":$k"] = $v;
+				}
+			}
+			$sql = substr($sql, 0, -2);
+			$sql .= " WHERE " . str_replace(':', ':__where__', $where_clause);
+			foreach ($where_fields as $k=>$v) {
+				$vars[":__where__$k"] = $v;
+			}
+			if ($sth = $this->query($sql, $vars)) {
+				return TRUE;
+			}
+		}
+		return FALSE;
+	}
+
+	/*
+	 *  Provide table name and associate array of field/value pairs
+	 *  Returns FALSE on failure
+	 */
+	function insert_or_update($table, $insert_fields, $update_fields) {
+		if (sizeof($insert_fields) && sizeof($update_fields)) {
+			$vars = array();
+			$sql = "INSERT INTO `$table` SET ";
+			foreach ($insert_fields as $k=>$v) {
+				// treat certain MySQL functions as safe
+				if ($v === 'NOW()') {
+					$sql .= "`$k` = NOW(), ";
+				} else if ($v === 'NULL') {
+					$sql .= "`$k` = NULL, ";
+				} else {
+					$sql .= "`$k` = :$k, ";
+					$vars[":$k"] = $v;
+				}
+			}
+			$sql = substr($sql, 0, -2);
+			$sql .= " ON DUPLICATE KEY UPDATE ";
+			foreach ($update_fields as $k=>$v) {
+				// treat certain MySQL functions as safe
+				if ($v === 'NOW()') {
+					$sql .= "`$k` = NOW(), ";
+				} else if ($v === 'NULL') {
+					$sql .= "`$k` = NULL, ";
+				} else {
+					$sql .= "`$k` = :$k, ";
+					$vars[":$k"] = $v;
+				}
+			}
+			$sql = substr($sql, 0, -2);
+			if ($sth = $this->query($sql, $vars)) {
+				return TRUE;
+			}
+		}
+		return FALSE;
+	}
+
+	/*
+	 *  Provide table name
+	 *  Enforces a where clause for protection (practically, it's always wanted anyway)
+	 *  Returns FALSE on failure
+	 */
+	function delete($table, $where_clause = false, $where_fields = array()) {
+		$vars = array();
+		$sql = "DELETE FROM `$table`";
+		if ($where_clause !== FALSE) {
+			$sql .= " WHERE " . $where_clause;
+		}
+		foreach ($where_fields as $k=>$v) {
+			$vars[":$k"] = $v;
+		}
+		if ($sth = $this->query($sql, $vars)) {
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+
+	/*
+	 *  !UTILITIES
+	 *
+	 *  Useful DB-related tools
+	 */
+
+	// ONLY to my used when a value can't supported above (E.g. WHERE field LIKE '%str%')
+	public function quote($str) {
+		return substr($this->dbh->quote($str), 1, -1);
+	}
+
+	// Convert a date from an dd/mm/yyyy input to yyyy-mm-dd
+	public function input_date_to_system_date($in) {
+		if (preg_match('/^\d{1,2}\/\d{1,2}\/\d{2,4}$/', $in)) {
+			$parts = explode('/', $in);
+			return str_pad($parts[2], 4, "20", STR_PAD_LEFT) . '-' . str_pad($parts[1], 2, "0", STR_PAD_LEFT) . '-' . str_pad($parts[0], 2, "0", STR_PAD_LEFT);
+		}
+		return FALSE;
+	}
+
+	// Convert a date from yyyy-mm-dd for dd/mm/yyyy input fields
+	public function system_date_to_input_date($in, $default_today = FALSE) {
+		if (preg_match('/^\d{4}-\d{2}-\d{2}$/', $in)) {
+			$parts = explode('-', $in);
+			return $parts[2] . '/' . $parts[1] . '/' . $parts[0];
+		} else if ($default_today === TRUE && empty($in)) {
+			return date("d/m/Y");
+		}
+		return '';
+	}
+
+
+	/*
+	 * !LOGGING
+	 */
+
+	function log_append($line) {
+		$this->log[] = $line;
+	}
+
+	function log_fetch() {
+		return $this->log;
+	}
+
+	function log_reset() {
+		$this->log=array();
+	}
+
+	function num_queries() {
+		return $this->num_queries;
+	}
+
+	function record_error($sql, $vars, $msg) {
+		if ($this->log_errors === TRUE) {
+			if (is_array($vars)) {
+				$vars = serialize($vars);
+			}
+			$this->insert('error_log', array(
+				'thedate' => 'NOW()',
+				'query' => $sql,
+				'vars' => $vars,
+				'message' => $msg,
+			));
+		}
+	}
+
+
+	// When the singleton object is serialized/unserialized
+	// the value of static variable will be lost
+	// so you need to re-assign to the static $instance
+	// the object by using restore_instance member (setter function)
+
+	public function restore_instance($session_instance) {
+		self::$instance = $session_instance;
+	}
+
+}
